7880d58ee6a7abd0be582073ba4f8102155080e4
ninaderi <nina8867@gmail.com>
2019-09-03 14:39:45 -0600

    updated build

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Layout,Header,Navigation,Drawer,Content} from 'react-mdl';
+import {Layout,Header,Navigation,Drawer,Content, Textfield} from 'react-mdl';
 import Home from './home';
 import {Link} from 'react-router-dom';
 import './App.css';
@@ -7,33 +7,50 @@ import './App.css';
 // import A from './a';
 
 class App extends React.Component {
+  gettext() {
+    var texting=document.getElementById('search').value;
+    if(texting=="Resume" || texting=="")
+    {
+      alert("Resume");
+    }
+    else{alert("Wrong");}
+
+  }
 render() {
  return (
      <div>
-   <div className="demo-big-content">
-   <Layout>
-       <Header className ="header-color" title="Niloufar Naderi" scroll >
-           <Navigation >
-               <Link to="/">About me</Link>
-               <Link to="/resume">Resume</Link>
-               {/* <Link to="/aboutme">About me</Link> */}
-               <Link to="/contact">Contact</Link>
-           </Navigation>
-       </Header>
-       <Drawer title="Title">
-           <Navigation>
-               <Link to="/main">About me</Link>
-               <Link to="/resume">Resume</Link>
-               {/* <Link to="/aboutme">About me</Link> */}
-               <Link to="/contact">Contact</Link>
-               </Navigation>
-       </Drawer>
-       <Content>
-           <div className="page-content" />
-           <Home/>
-       </Content>
-   </Layout>
-</div>
+         <div className="demo-big-content">
+         <Layout>
+             <Header className ="header-color" title="Niloufar Naderi" scroll >
+               <Textfield
+                  
+                    id="search"
+                    onClick={this.gettext}
+                    label="Search3"
+                    expandable
+                    expandableIcon={"search-100"}
+                />
+                 <Navigation >
+                     <Link to="/">About me</Link>
+                     <Link to="/resume">Resume</Link>
+                     {/* <Link to="/aboutme">About me</Link> */}
+                     <Link to="/contact">Contact</Link>
+                 </Navigation>
+             </Header>
+             <Drawer title="Title">
+                 <Navigation>
+                     <Link to="/main">About me</Link>
+                     <Link to="/resume">Resume</Link>
+                     {/* <Link to="/aboutme">About me</Link> */}
+                     <Link to="/contact">Contact</Link>
+                  </Navigation>
+             </Drawer>
+             <Content>
+                 <div className="page-content" />
+                 <Home/>
+             </Content>
+         </Layout>
+      </div>
 </div> );
 }
 }
